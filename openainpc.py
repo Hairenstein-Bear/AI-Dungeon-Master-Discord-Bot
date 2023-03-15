458153dc0e84ebfc4338cc0fdc762d37de56d490
Hairenstein-Bear <paul.hair917@gmail.com>
2023-03-15 17:42:34 -0400

    added ability for bot to help with rules questions

--- openainpc.py
+++ openainpc.py
@@ -8,6 +8,25 @@ import config
 # this function is all chatGPT stuff
 
 
+def rule(question):
+    openai.api_key = config.openaikey
+    MODEL = "gpt-3.5-turbo"
+    # temperature is the variable that sets how complex the response can be the higher the number the higher the complexity
+    temperature = 0.5
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": "You are the dungeon master for a fifth edition dungeons and dragons game, your players are all new and don't understand how to play the game. Answer their questions as completely and accurately as possible."},
+            {"role": "user", "content": "Answer this question: " + question}
+            # {"role": "system", "content": "You are a dungeons and dragons dm creating non player characters in your world most of the time your non player characters dont have character classes but instead have occupations"},
+            # {"role": "user", "content": "Create a backstory for a non-player character in my Dungeons and Dragons game who has the following characteristics:" + characteristics +
+            #     ". This character should have an occupation that makes sense given their background and personality. Please avoid making them a blacksmith unless it's the most fitting occupation based on the provided information."}
+        ],
+        temperature=temperature,
+    )
+    return response['choices'][0]['message']['content']
+
+
 def getNpc(characteristics):
     openai.api_key = config.openaikey
     MODEL = "gpt-3.5-turbo"
@@ -38,13 +57,23 @@ async def on_ready():
 # channels where I might use the bot
 
 
-@bot.slash_command(guild_ids=[639278542382694401, 348564060846948363, 934954222829584435, 878487240383295549])
+@bot.slash_command(description="Enter desired characteristics using commas between each. (e.g. race human, profession barber)", guild_ids=[639278542382694401, 348564060846948363, 934954222829584435, 878487240383295549])
 async def npc(interaction: nextcord.Interaction, characteristics: str):
     # defer the response, so we can take a long time to respond, by default discord give you 3 seconds to send a response
     await interaction.response.defer()
     # call getNpc to generate the character this takes about 5-10 seconds usually sometimes longer
     npc = getNpc(characteristics)
     # followup must be used after defer since a response is already sent this updates the message to include the generated character
-    await interaction.followup.send(npc)
+    await interaction.followup.send(npc + " \n\nCharacteristics specified: " + characteristics)
+
+
+@bot.slash_command(description="Type a rules question", guild_ids=[639278542382694401, 348564060846948363, 934954222829584435, 878487240383295549])
+async def rules(interaction: nextcord.Interaction, question: str):
+    # defer the response, so we can take a long time to respond, by default discord give you 3 seconds to send a response
+    await interaction.response.defer()
+    # call getNpc to generate the character this takes about 5-10 seconds usually sometimes longer
+    answer = rule(question)
+    # followup must be used after defer since a response is already sent this updates the message to include the generated character
+    await interaction.followup.send("Question asked: " + question + "\n\n" + answer)
 
 bot.run(config.botkey)
